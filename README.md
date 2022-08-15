8147b6cbca5ab89b78f94f53f5bcfef6956df865
augustinecyr <augustinecyr@gmail.com>
2022-08-15 14:13:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Visa Technology Traineeship Program
+
+### Mini Project
+
+### Week 1 
+1. Setting up project files and folders
+2. Add all standard dependencies and required properties on VS.
+3. Set remote connections to GitHub and Heroku.
+4. Brainstorming for ideas and gathering reliable API sources.
---
+++ src/main/java/com/sg/vttpminiproject/VttpminiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sg.vttpminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpminiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpminiprojectApplication.class, args);
+	}
+
+}
