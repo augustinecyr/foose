4c21fc7404bb5b71ee0e39f781ce33a80820164e
augustinecyr <augustinecyr@gmail.com>
2022-08-30 12:40:49 +0800

    stopped trfrepo

--- src/main/java/com/sg/vttpminiproject/services/TransferMarktService.java
+++ src/main/java/com/sg/vttpminiproject/services/TransferMarktService.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
- import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.sg.vttpminiproject.models.TransferMarkt;
- import com.sg.vttpminiproject.repositories.TransferMarktRepository;
-
+// import com.sg.vttpminiproject.repositories.TransferMarktRepository;
 
 import jakarta.json.Json;
 import jakarta.json.JsonArray;
@@ -27,14 +26,15 @@ import jakarta.json.JsonValue;
 
 @Service
 public class TransferMarktService {
-    private static final String URL = "https://transfermarket.p.rapidapi.com/competitions/get-table";
-	// https://transfermarket.p.rapidapi.com/competitions/get-table?id=GB1&seasonID=2022 // example for EPL table.
+	private static final String URL = "https://transfermarket.p.rapidapi.com/competitions/get-table";
+	// https://transfermarket.p.rapidapi.com/competitions/get-table?id=GB1&seasonID=2022
+	// // example for EPL table.
 
 	@Value("${X-RapidAPI-Key}")
 	private String rapidAPI;
 
-    @Autowired
-	 private TransferMarktRepository trfRepo;
+	// @Autowired
+	// private TransferMarktRepository trfRepo;
 
 	public List<TransferMarkt> getTable(String id, String seasonID) {
 
@@ -46,13 +46,13 @@ public class TransferMarktService {
 			String url = UriComponentsBuilder
 					.fromUriString(URL)
 					.queryParam("id", id)
-                    .queryParam("seasonID", seasonID)
+					.queryParam("seasonID", seasonID)
 					.encode()
 					.toUriString();
 
 			RequestEntity<Void> req = RequestEntity
 					.get(url)
-                    .header("X-RapidAPI-Key","%s".formatted(rapidAPI))// rapidAPI key
+					.header("X-RapidAPI-Key", "%s".formatted(rapidAPI))// rapidAPI key
 					.build();
 
 			System.out.println(">>> [url]: " + url); // prints out the URL that is built
@@ -70,7 +70,7 @@ public class TransferMarktService {
 			System.out.println("-----------------------------------------------------------");
 
 		} catch (Exception ex) {
-			System.err.printf("Error: %s\n", ex.getMessage()); // troubleshooting 
+			System.err.printf("Error: %s\n", ex.getMessage()); // troubleshooting
 			return Collections.emptyList();
 		}
 
@@ -80,22 +80,20 @@ public class TransferMarktService {
 			JsonReader r = Json.createReader(strReader);
 			JsonObject j = r.readObject();
 
-            JsonArray leagueTable = j.getJsonArray("table");
-            JsonObject first = leagueTable.getJsonObject(0); // first team of the table
-            JsonString firstName = first.getJsonString("clubName"); 
-            JsonNumber points = first.getJsonNumber("points");
-            JsonObject last = leagueTable.getJsonObject(19);
-            JsonString lastName = last.getJsonString("clubName");
+			JsonArray leagueTable = j.getJsonArray("table");
+			JsonObject first = leagueTable.getJsonObject(0); // first team of the table
+			JsonString firstName = first.getJsonString("clubName");
+			JsonNumber points = first.getJsonNumber("points");
+			JsonObject last = leagueTable.getJsonObject(19);
+			JsonString lastName = last.getJsonString("clubName");
 
 			System.out.println("Total number of teams: " + leagueTable.size());
-            System.out.println("-----------------------------------------------------------");
-            System.out.println("Top of the table: " + firstName);
-            System.out.println("Points: " + points);
-            System.out.println("-----------------------------------------------------------");
-            System.out.println("Bottom of the table: " + lastName);
-            System.out.println("-----------------------------------------------------------");
-
-
+			System.out.println("-----------------------------------------------------------");
+			System.out.println("Top of the table: " + firstName);
+			System.out.println("Points: " + points);
+			System.out.println("-----------------------------------------------------------");
+			System.out.println("Bottom of the table: " + lastName);
+			System.out.println("-----------------------------------------------------------");
 
 			for (JsonValue v : j.getJsonArray("table")) {
 
@@ -103,10 +101,9 @@ public class TransferMarktService {
 
 			}
 
-
 		}
 
-		trfRepo.save(tables);
+		// trfRepo.save(tables);
 
 		return tables;
 	}
