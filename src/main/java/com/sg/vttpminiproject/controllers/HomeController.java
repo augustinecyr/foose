9e67ebcc29b593649937967e6d3087da3370beb6
augustinecyr <augustinecyr@gmail.com>
2022-09-24 14:24:48 +0800

    remapping with controller

---
+++ src/main/java/com/sg/vttpminiproject/controllers/HomeController.java
@@ -0,0 +1,34 @@
+package com.sg.vttpminiproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+	public String home() {
+		
+		return "index";
+	}
+
+    @GetMapping("/about")
+	public String aboutUs() {
+		
+		return "about";
+	}
+
+    @GetMapping("/searchtable")
+	public String searchTable() {
+		
+		return "searchtable";
+	}
+
+    @GetMapping("/contactus")
+	public String contactUs() {
+		
+		return "contact";
+	}
+
+
+}
