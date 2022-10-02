8a501f7e95d29ad17bd1c7fc396498ac7912737e
augustinecyr <augustinecyr@gmail.com>
2022-10-02 15:51:35 +0800

    added datetime

--- src/main/java/com/sg/vttpminiproject/controllers/ContactController.java
+++ src/main/java/com/sg/vttpminiproject/controllers/ContactController.java
@@ -1,9 +1,11 @@
 package com.sg.vttpminiproject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping(path = "/submitform")
 public class ContactController {
     
 }
