c7bcdf52fc1674814bdd0517f1c013373e6290f3
augustinecyr <augustinecyr@gmail.com>
2022-10-05 21:30:50 +0800

    test /contactrest

--- src/main/java/com/sg/vttpminiproject/controllers/ContactRestController.java
+++ src/main/java/com/sg/vttpminiproject/controllers/ContactRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +20,12 @@ public class ContactRestController {
     private ContactRepository conRepo;
 
     @GetMapping(path = "/contactrest/{email}", produces = "application/json")
-    public ResponseEntity<String> getMsg(@PathVariable String email) {
+    public ResponseEntity<String> getMsg(@PathVariable String email, @ModelAttribute Optional<Contact> contact) {
 
-        Optional<Contact> existingEntry = conRepo.get(email);
+        contact = conRepo.get(email);
 
-        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(existingEntry.toString());
-    
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(contact.toString());
+
     }
 
 }
